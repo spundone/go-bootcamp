309e8a17166a74ab4b4af8b9b1466b35c0d07e22
Spandan <spandan@one2n.in>
2025-02-14 12:16:43 +0530

    Remove 'filter' prefix from all number filtering function names - Update main function to use new function names
    Signed-off-by: Spandan <spandan@one2n.in>

--- number-filter/main.go
+++ number-filter/main.go
@@ -4,8 +4,8 @@ import (
     "fmt"
 )
 
-// filterEvenNumbers filters out even numbers from a slice of integers.
-func filterEvenNumbers(numbers []int) []int {
+// evenNumbers filters out even numbers from a slice of integers.
+func evenNumbers(numbers []int) []int {
     var evens []int
     for _, num := range numbers {
         if num%2 == 0 {
@@ -15,8 +15,8 @@ func filterEvenNumbers(numbers []int) []int {
     return evens
 }
 
-// filterOddNumbers filters out odd numbers from a slice of integers.
-func filterOddNumbers(numbers []int) []int {
+// oddNumbers filters out odd numbers from a slice of integers.
+func oddNumbers(numbers []int) []int {
     var odds []int
     for _, num := range numbers {
         if num%2 != 0 {
@@ -40,8 +40,8 @@ func isPrime(num int) bool {
 }
 
 
-// filterPrimeNumbers filters out prime numbers from a slice of integers.
-func filterPrimeNumbers(numbers []int) []int {
+// primeNumbers filters out prime numbers from a slice of integers.
+func primeNumbers(numbers []int) []int {
 	var primes []int
 	for _, num := range numbers {
 		if isPrime(num) {
@@ -51,8 +51,8 @@ func filterPrimeNumbers(numbers []int) []int {
 	return primes
 }
 
-// filterOddPrimeNumbers filters out odd prime numbers from a slice of integers.
-func filterOddPrimeNumbers(numbers []int) []int {
+// oddPrimeNumbers filters out odd prime numbers from a slice of integers.
+func oddPrimeNumbers(numbers []int) []int {
 	var odds []int
 	for _, num := range numbers {
 		if isPrime(num) && num%2 != 0 {
@@ -62,8 +62,8 @@ func filterOddPrimeNumbers(numbers []int) []int {
 	return odds
 }
 
-// filterEvenMultiplesOf5 filters out even multiples of 5 from a slice of integers.
-func filterEvenMultiplesOf5(numbers []int) []int {
+// evenMultiplesOf5 filters out even multiples of 5 from a slice of integers.
+func evenMultiplesOf5(numbers []int) []int {
 	var evens []int
 	for _, num := range numbers {
 		if num%2 == 0 && num%5 == 0 {
@@ -73,8 +73,8 @@ func filterEvenMultiplesOf5(numbers []int) []int {
 	return evens
 }
 
-// filterOddMultiplesOf3 filters out odd multiples of 3 from a slice of integers greater than 10.
-func filterOddMultiplesOf3(numbers []int) []int {
+// oddMultiplesOf3 filters out odd multiples of 3 from a slice of integers greater than 10.
+func oddMultiplesOf3(numbers []int) []int {
 	var odds []int
 	for _, num := range numbers {
 		if num%2 != 0 && num%3 == 0 && num > 10 {	
@@ -84,8 +84,8 @@ func filterOddMultiplesOf3(numbers []int) []int {
 	return odds
 }
 
-// filterNumbers filters out numbers from a slice of integers that match all the conditions.
-func filterNumbers(numbers []int, conditions []func(int) bool) []int {
+// numbers filters out numbers from a slice of integers that match all the conditions.
+func numbers(numbers []int, conditions []func(int) bool) []int {
 	var filtered []int
 	for _, num := range numbers {
 		matchesAll := true
@@ -106,10 +106,11 @@ func filterNumbers(numbers []int, conditions []func(int) bool) []int {
 func main() {
 	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 	numbersExtended := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
-	fmt.Println("Even numbers:", filterEvenNumbers(numbers))
-	fmt.Println("Odd numbers:", filterOddNumbers(numbers))
-	fmt.Println("Prime numbers:", filterPrimeNumbers(numbers))
-	fmt.Println("Odd prime numbers:", filterOddPrimeNumbers(numbers))
-	fmt.Println("Even multiples of 5:", filterEvenMultiplesOf5(numbersExtended))
-	fmt.Println("Odd multiples of 3 greater than 10", filterOddMultiplesOf3(numbersExtended))
+	fmt.Println("Even numbers:", evenNumbers(numbers))
+	fmt.Println("Odd numbers:", oddNumbers(numbers))
+	fmt.Println("Prime numbers:", primeNumbers(numbers))
+	fmt.Println("Odd prime numbers:", oddPrimeNumbers(numbers))
+	fmt.Println("Even multiples of 5:", evenMultiplesOf5(numbersExtended))
+	fmt.Println("Odd multiples of 3 greater than 10", oddMultiplesOf3(numbersExtended))
+	//fmt.Println("Even multiples of 5 and odd multiples of 3", numbers(numbersExtended, []func(int) bool{evenMultiplesOf5, oddMultiplesOf3}))
 }
